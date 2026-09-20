3c25e11d88acfceb4b7eb415c6ce8c9e8d01155e
Shwei <portal.gamy@gmail.com>
2026-09-20 21:26:24 +0300

    fix hw1_6 bug

--- sem7/HW1_6/part2/t02.kts
+++ sem7/HW1_6/part2/t02.kts
@@ -5,20 +5,28 @@ fun generateRandomArray(size: Int, maxValue: Int): IntArray? {
     return IntArray(size) { Random.nextInt(0, maxValue + 1) }
 }
 
-generateRandomArray(size = 10, maxValue = 50)
-    ?.apply {
-        for (i in indices) {
-            if (this[i] % 2 == 0) this[i] / 2 else this[i] * 2
+fun main() {
+    generateRandomArray(size = 10, maxValue = 50)
+        ?.let { array ->
+            for (i in array.indices) {
+                array[i] = if (array[i] % 2 == 0) {
+                        array[i] / 2 }
+                                                  else {
+                        array[i] * 2                                            
+                    } 
+                } 
+            array
         }
-    }
-    .also {
-        println("${it.contentToString()}")
-    }
-    ?.run {
-        maxOrNull()
-    }
-    .also {
-        "$it"
-    }
-?: "Помилка вхідних даних\n"
+        .also {
+            println("${it.contentToString()}")
+        }
+        ?.run {
+            maxOrNull()
+        }
+        .also {
+            println("$it")
+        }
+    ?: println("Помилка вхідних даних")
+}
 
+main()
